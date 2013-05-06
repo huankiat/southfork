7d3e0fcd42250c584839dd7e006ec0a1122d00d2
Huankiat Seh <huankiat@digitalrojak.com>
2013-05-06 13:51:30 +0800

    Absolute link working

--- southfork/settings_hks.py
+++ southfork/settings_hks.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/Shared/Dropbox/sqlite3.db',  # Or path to database file if using sqlite3.
+        'NAME': '/Users/huankiat/Dropbox/VFNexus/Cloud_Manufacturing/southfork_dev_db/sqlite3.db',  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
